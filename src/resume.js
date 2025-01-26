20ef660086a8d7f35fba80224a4f6e6145e4dd65
maxdup <mdupuis@hotmail.ca>
2025-01-25 20:31:54 -0500

    added photo

--- src/resume.js
+++ src/resume.js
@@ -1,2 +1,8 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./style/resume.scss";
+import imageUrl from "./assets/maxdup.png";
+
+const img = document.createElement("img");
+img.setAttribute("src", imageUrl);
+img.setAttribute("id", "profile-photo");
+document.querySelector("#header").appendChild(img);
